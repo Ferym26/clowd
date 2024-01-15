67f9b83ff7ec7407bfc894f4c93b64bd18ac10e0
ferym26 <ferym26@gmail.com>
2024-01-16 00:01:15 +0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.same
+	p(v-for="(user, index) in users" :key="index") {{ user.name }}
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			users: [],
+		}
+	},
+	mounted() {
+		this.getData();
+	},
+	methods: {
+		getData() {
+			fetch('https://jsonplaceholder.typicode.com/users')
+				.then(response => response.json())
+				.then(json => this.users = json)
+		},
+	}
+}
+</script>
